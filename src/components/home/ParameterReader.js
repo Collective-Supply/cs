0967759591e154239e7e203e89c2ee3f60e64948
dev <you@example.comhangme@gmail.com>
2022-03-25 05:08:25 -0400

    blah

--- src/components/home/ParameterReader.js
+++ src/components/home/ParameterReader.js
@@ -6,19 +6,20 @@ module.exports = (parameterName) => {
     const urlParams = new URLSearchParams(queryString);
     const LinkId = urlParams.get(parameterName);
     
-    // *** COOKIE SETTINGS - replace if removing login with popup instead of redirect in LoginButton.js
-    // Set a cookie to remember the URL param to recall when user signs in
-    // Keep in mind that the cookie is only set for this domain so once you move 
-    // shared or profile director over, it will need to be updated!
-    if (LinkId) {
-        document.cookie = `urlParam=${LinkId}`;
-    };
-    // Parses out the urlParam part of the cookie
-    // https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie
-    const urlParamCookie = document.cookie
-        .split('; ')
-        .find(row => row.startsWith('urlParam='))
-        .split('=')[1];
-    // returns the url parameter   
-    return urlParamCookie;
+    // // *** COOKIE SETTINGS - replace if removing login with popup instead of redirect in LoginButton.js
+    // // Set a cookie to remember the URL param to recall when user signs in
+    // // Keep in mind that the cookie is only set for this domain so once you move 
+    // // shared or profile director over, it will need to be updated!
+    // if (LinkId) {
+    //     document.cookie = `urlParam=${LinkId}`;
+    // };
+    // // Parses out the urlParam part of the cookie
+    // // https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie
+    // const urlParamCookie = document.cookie
+    //     .split('; ')
+    //     .find(row => row.startsWith('urlParam='))
+    //     .split('=')[1];
+    // // returns the url parameter
+    // console.log(urlParamCookie)   
+    // return urlParamCookie;
 };
